978da587a0b54d0a4d5ce66ff45705d388588426
cl0ud08 <Harshit.105418@stu.upes.ac.in>
2025-06-04 13:42:44 +0530

    read file in system

--- NODEJS/fileCRUD.js
+++ NODEJS/fileCRUD.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
 
-//fs.writeFileSync('file.txt', 'MY name is harshit');
-fs.writeFile('file.txt', 'MY name is harshit' ,err=>{});
+// fs.writeFileSync('file.txt', 'MY name is harshit');
+// fs.writeFile('file.txt', 'MY name is harshit', err => {});
+
+fs.readFile('./file.txt', 'utf-8', (err, data) => {
+    if (err){
+        console.log(err);}
+    else console.log(data);
+});
