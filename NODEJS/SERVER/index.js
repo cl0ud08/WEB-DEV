195421267201364dc543862da9d605c36c02f5c6
cl0ud08 <Harshit.105418@stu.upes.ac.in>
2025-06-05 12:37:59 +0530

    add SERVER folder

--- NODEJS/SERVER/index.js
+++ NODEJS/SERVER/index.js
@@ -2,12 +2,15 @@
 
 const http = require('http');
 const fs =require('fs');
+const url = require('url');
 
 // append all the logs to a file called log.txt
 //create a server that listens on port 8000
 // and responds with ""herllo from serve"
 const myServer = http.createServer((req, res) => {
     const log= `${Date.now()}: New request recieved\n`;
+    const myurl = url.parse(req.url);
+    console.log(myurl);
     fs.appendFile('log.txt',log,(err,data)=>{
         res.end("hello from server");
     })
