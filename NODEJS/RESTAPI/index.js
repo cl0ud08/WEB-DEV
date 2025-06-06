b74e8bea59af95bf2f7c685db8ba91e7e288f6a9
cl0ud08 <Harshit.105418@stu.upes.ac.in>
2025-06-06 13:27:38 +0530

    add middleware

--- NODEJS/RESTAPI/index.js
+++ NODEJS/RESTAPI/index.js
@@ -4,6 +4,15 @@ const app = express();
 
 const port = 8000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Logging middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 // Routes
 app.get('/api/users', (req, res) => {
     return res.json(users);
@@ -18,26 +27,26 @@ app.get('/users', (req, res) => {
     res.send(html);
 });
 
-app.get('/api/users/:id', (req,res)=>{
+app.get('/api/users/:id', (req, res) => {
     const id = Number(req.params.id);
-    const user =users.find((user)=>user.id===id);
+    const user = users.find((user) => user.id === id);
     return res.json(user);
-
 });
-app.post('.api/users', (req,res)=>{
-    //
+
+app.post('/api/users', (req, res) => {
+    // Access new user data with req.body
     return res.json();
 });
 
-app.patch('.api/users/:id', (req,res)=>{
-    //
+app.patch('/api/users/:id', (req, res) => {
+    // Access update data with req.body
     return res.json();
 });
 
-app.delete('.api/users/:id', (req,res)=>{
-    //
+app.delete('/api/users/:id', (req, res) => {
     return res.json();
 });
+
 app.listen(port, () => {
-    console.log('server started on http://localhost:' + port );
+    console.log('server started on http://localhost:' + port);
 });
